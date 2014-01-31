1db0677cdb7e829787e27ff2dbe8b4c7f031085e
KoGor <kg@source.fake>
2014-01-31 16:45:13 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Map Icons Generator (MIG)
+=========================
+
+MIG is simple tool for generating icons (both vector and raster) based on cartographic data. It is quite flexible, you can change color, lines width, margins and of course size of icons. As far as it based on GeoData ([TopoJSON](https://github.com/mbostock/topojson/wiki)) you can easily change projection and it's parameters or filter regions by geo conditions. And you can feed to it any map: world map, country map with administrative borders or even your town map with districts borders.
+
+## How to use
+By default MIG generates icons for 174 world countries, GeoData is 1:110m [natural earth data](http://www.naturalearthdata.com/downloads/). If it's just what you want you can change some general parameters directly in the code or with help of GUI on [demopage](#).
+
+If you want more - you can create another input GeoData file and change couple of corresponding code lines. All is quite easy. First of all you have to create [TopoJSON](https://github.com/mbostock/topojson/wiki) file. Make it with help of [Command-line TopoJSON](https://github.com/mbostock/topojson/wiki/Command-Line-Reference) application. If you don't have proper data for naming icons inside TopoJSON file - use external .csv or .tsv file. So now you have new input file(s), so update paths for them in the code:
+```js
+  .defer(d3.json, "data/TopoJSON_file.json")
+  .defer(d3.tsv, "data/external_file_for_names.tsv")
+```
+Next step is to change feature name according to your new TopoJSON file:
+```js
+.data(topojson.feature(world, world.objects.YourNewFeatureName).features
+```
+That's all. Now you should be able to generate icons from other GeoData.
+
+## Advanced adjustments
+Sometimes it is better to use not standard projection, in these cases you can use one of [avaliable projections](https://github.com/mbostock/d3/wiki/Geo-Projections) or even create your own. So feel free to experiment.
+
+## Further improvements
+Add ability for line simplification and quantization as soon as it will be available in [topojson.js](https://github.com/mbostock/topojson). Now you can make it only on the stage of creating TopoJSON file. Improve demopage's GUI, add control for filtering GeoData.
+
+## Thanks
+Thanks to [Mike Bostock](http://bost.ocks.org/mike/) for great tool &mdash; [D3.js](http://d3js.org/) and to [Jason Davies](http://www.jasondavies.com/) for inspiring example &mdash; [Countries by area](http://www.jasondavies.com/maps/countries-by-area/).
---
+++ js/MapIconsGenerator.js
@@ -0,0 +1,188 @@
+//Change variable for different output
+var iconSize = "72",
+    iconMargins = 3,
+    fillColor = "#636363",
+    borderColor = "#000000",
+    borderWidth = "1px", 
+    //contryList = [643, 840], //list of ids for filtering
+    tooltip = d3.select("body").append("div").attr("class", "tooltip");
+
+var vectorSave = d3.select("button#vectorSave"),
+    rasterSave = d3.select("button#rasterSave");
+
+    vectorSave.on("click", function() {
+      var icons = d3.select("#vectorPreview").selectAll("svg");
+      icons.each(saveAsSVG);
+    });
+
+    rasterSave.on("click", function() {
+      var icons = d3.select("#rasterPreview").selectAll("canvas");
+      icons.each(saveAsPNG);
+    });
+
+queue()
+  .defer(d3.json, "data/world-110m.json")
+  .defer(d3.tsv, "data/world-110m-country-names.tsv")
+  .await(generator);
+
+function generator(error, world, countries) {
+  var countryById = {};
+
+  countries.forEach(function(d) {
+    countryById[d.id] = d.name;
+  });
+
+  //Vector icons
+  generateVector(world, countries, countryById);
+
+  //Rasterise icons
+  icons = d3.select("#vectorPreview").selectAll("svg");
+  icons.each(generateRaster);
+          
+};
+
+function generateVector(world, countries, countryById) {
+  var svg = d3.select("#vectorPreview").selectAll("svg")
+      .data(topojson.feature(world, world.objects.countries).features
+      //.data(topojson.feature(world, world.objects.countries).features.filter(function(d) {return contryList.indexOf(d.id) > -1;}))
+    .enter().append("svg")
+      .each(function(d) {
+        //Set projections
+        switch (d.id) {
+          //Russia
+          case 643: var projection = d3.geo.albers().rotate([-105, 0]).center([-10, 65]).parallels([52, 64]);
+                      path = d3.geo.path().projection(projection);
+                      break;
+          //USA
+          case 840: var projection = d3.geo.albersUsa();
+                      path = d3.geo.path().projection(projection);
+                      break;
+          default:    var projection = d3.geo.mercator();
+                      path = d3.geo.path().projection(projection);
+                      break;
+        }
+        var svg = d3.select(this),
+            bounds = path.bounds(d),
+            dx = bounds[1][0] - bounds[0][0],
+            dy = bounds[1][1] - bounds[0][1],
+            dmax = Math.max(dx, dy),
+            s = 1 / Math.max(dx / (iconSize - 2 * iconMargins), dy / (iconSize - 2 * iconMargins));
+        svg 
+            .attr("width", iconSize)
+            .attr("height", iconSize)
+            .attr("overflow", "hidden")
+            .attr("id", countryById[d.id])
+          .append("g")
+            .attr("transform", "scale(" + s + ")translate(" + [iconMargins/s + (dmax - dx) * 0.5 - bounds[0][0], iconMargins/s + (dmax - dy) * 0.5 - bounds[0][1]] + ")")
+          .append("path")
+            .style("fill", fillColor)
+            .style("stroke", borderColor)
+            .style("stroke-width", borderWidth)
+            .attr("d", path);
+      })
+      .sort(function(a, b) { return a.id - b.id; })
+      .on("click", function() {console.log(this.id);})
+      .on("mouseover", function(d, i) {
+        var t = tooltip.html("").style("display", "block");
+        t.append("span").attr("class", "country").text(countryById[d.id]);
+      })
+      .on("mouseout", function() {
+        tooltip.style("display", null);
+      });
+}
+
+function generateRaster(svg) {
+  var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+      svg = d3.select(this),
+      id = svg.attr("id"),
+      iconSize = Math.max(svg.attr("width"), svg.attr("height"));
+
+  xmlnsFix(svg);
+
+  var source = (new XMLSerializer()).serializeToString(svg.node()),
+      url = window.URL.createObjectURL(new Blob([doctype + source], { "type" : "image\/svg+xml" }));
+
+  var canvas = d3.select("#rasterPreview").append("canvas")
+        .attr("overflow", "hidden")
+        .attr("id", id + ".png")
+        .attr("width", iconSize)
+        .attr("height", iconSize);
+
+  var context = canvas.node().getContext("2d");
+  
+  canvas
+    .on("click", function() {console.log(this.id);})
+    .on("mouseover", function() {
+      id = this.id;
+      var t = tooltip.html("").style("display", "block");
+      t.append("span").attr("class", "country").text((id.split("."))[0]);
+    })
+    .on("mouseout", function() {
+      tooltip.style("display", null);
+    });
+  
+  var image = new Image;
+  image.src = url;
+  image.onload = function() {
+    context.drawImage(image, 0, 0, iconSize, iconSize);
+  };
+
+  setTimeout(function() {
+    window.URL.revokeObjectURL(url);
+  }, 10);
+};
+
+function saveAsSVG(svg) {
+  var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+      svg = d3.select(this),
+      id = svg.attr("id");
+
+  xmlnsFix(svg);
+  
+  var source = (new XMLSerializer()).serializeToString(svg.node()),
+      url = window.URL.createObjectURL(new Blob([doctype + source], { "type" : "image\/svg+xml" }));
+
+  var a = d3.select("body")
+        .append('a')
+        .attr("class", "mapIcon-generator")
+        .attr("download", id + ".svg")
+        .attr("href", url)
+        .style("display", "none");
+
+  a.node().click();
+
+  setTimeout(function() {
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }, 10);
+};
+
+function saveAsPNG() {
+  var a = d3.select("body")
+        .append('a')
+        .attr("class", "mapIcon-generator")
+        .attr("download", this.id)
+        .attr("href", this.toDataURL("image/png"))
+        .style("display", "none");
+
+  a.node().click();
+
+  setTimeout(function() {
+    a.remove();
+  }, 10);
+};
+
+function xmlnsFix(svg) {
+  // removing attributes so they aren't doubled up
+  svg.node().removeAttribute("xmlns");
+  svg.node().removeAttribute("xlink");
+
+  // These are needed for for valid SVG 1.1
+  if (!svg.node().hasAttributeNS(d3.ns.prefix.xmlns, "xmlns")) {
+    svg.node().setAttributeNS(d3.ns.prefix.xmlns, "xmlns", d3.ns.prefix.svg);
+  }
+
+  if (!svg.node().hasAttributeNS(d3.ns.prefix.xmlns, "xmlns:xlink")) {
+    svg.node().setAttributeNS(d3.ns.prefix.xmlns, "xmlns:xlink", d3.ns.prefix.xlink);
+  }
+};
